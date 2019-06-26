Organizing the code and fixing the Jaccard calculation

main.py
@@ -1,134 +1,119 @@
-from imutils.video import VideoStream
-from imutils.video import FPS
-import argparse
-import imutils
-import time
-import cv2
-import os
-import numpy as np 
-
-ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video", type=str, default="car1",
-    help="path to input video file")
-ap.add_argument("-t", "--tracker", type=str, default="kcf",
-    help="OpenCV object tracker type")
-args = vars(ap.parse_args())
+from utils import *
 
 
-# extract the OpenCV version info
-(major, minor) = cv2.__version__.split(".")[:2]
+def main1(ap, args):
+    # extract the OpenCV version info
+    (major, minor) = cv2.__version__.split(".")[:2]
 
-# if we are using OpenCV 3.2 OR BEFORE, we can use a special factory
-# function to create our object tracker
-if int(major) == 3 and int(minor) < 3:
-    tracker = cv2.Tracker_create(args["tracker"].upper())
+    # if we are using OpenCV 3.2 OR BEFORE, we can use a special factory
+    # function to create our object tracker
+    if int(major) == 3 and int(minor) <3:
+        tracker = cv2.Tracker_create(args["tracker"].upper())
 
-# otherwise, for OpenCV 3.3 OR NEWER, we need to explicity call the
-# approrpiate object tracker constructor:
-else:
-    # initialize a dictionary that maps strings to their corresponding
-    # OpenCV object tracker implementations
-    OPENCV_OBJECT_TRACKERS = {
-        "csrt": cv2.TrackerCSRT_create,
-        "kcf": cv2.TrackerKCF_create,
-        "boosting": cv2.TrackerBoosting_create,
-        "mil": cv2.TrackerMIL_create,
-        "tld": cv2.TrackerTLD_create,
-        "medianflow": cv2.TrackerMedianFlow_create,
-        "mosse": cv2.TrackerMOSSE_create
-    }
+    # otherwise, for OpenCV 3.3 OR NEWER, we need to explicity call the
+    # approrpiate object tracker constructor:
+    else:
+        # initialize a dictionary that maps strings to their corresponding
+        # OpenCV object tracker implementations
+        OPENCV_OBJECT_TRACKERS = {
+            "csrt": cv2.TrackerCSRT_create,
+            "kcf": cv2.TrackerKCF_create,
+            "mosse": cv2.TrackerMOSSE_create,
+            "medianflow": cv2.TrackerMedianFlow_create
 
-    # grab the appropriate object tracker using our dictionary of
-    # OpenCV object tracker objects
-    tracker = OPENCV_OBJECT_TRACKERS[args["tracker"]]()
+        }
 
-# if a video path was not supplied, grab the reference to the web cam
-#if not args.get("video", False):
-#   print("[INFO] starting video stream...")
-#   vs = VideoStream(src=0).start()
-#   time.sleep(1.0)
+        # grab the appropriate object tracker using our dictionary of
+        # OpenCV object tracker objects
+        tracker = OPENCV_OBJECT_TRACKERS[args["tracker"]]()
 
-# otherwise, grab a reference to the video file
-path = '../cars/' + args.get("video")
-gt = np.loadtxt('../cars/gt' + args.get("video") + '.txt', delimiter=',')
-print(gt)
-all_files = os.listdir(path)
-all_files = sorted(list(set([filename for filename in all_files])))
-all_files.remove('.DS_Store')
 
-# initialize the bounding box coordinates of the object we are going
-# to track
-initBB = tuple(gt[0].astype(int))
+    # otherwise, grab a reference to the video file
+    path = '../cars/' + args.get("video")
+    gt = np.loadtxt('../cars/gt' + args.get("video") + '.txt', dtype=float, delimiter=',')
+    all_files = os.listdir(path)
+    all_files = sorted(list(set([filename for filename in all_files])))
+    if all_files[0] == '.DS_Store':
+        all_files.remove('.DS_Store')
 
-# initialize the FPS throughput estimator
-fps = None
+    initBB = tuple(gt[0])
 
-# loop over frames from the video stream
+    aux2 = 0
+    totalJack = np.zeros((1))
+    error = 0
+    for f in all_files:
+        frame = cv2.imread(path + '/' + f)
 
-for f in all_files:
-    # grab the current frame, then handle if we are using a
-    # VideoStream or VideoCapture object
-    frame = cv2.imread(path + '/' + f)
-    
-    # check to see if we have reached the end of the stream
-    
-    if frame is None:
-        break
+        if frame is None:
+            break
 
-    # resize the frame (so we can process it faster) and grab the
-    # frame dimensions
-    frame = imutils.resize(frame, width=500)
-    (H, W) = frame.shape[:2]
+        if not math.isnan(gt[aux2][0]):
+            cv2.rectangle(frame, (int(gt[aux2][0]), int(gt[aux2][1])), 
+                (int(gt[aux2][2]),  int(gt[aux2][3])),
+                (0, 0, 255), 2)
+        (H, W) = frame.shape[:2]
+        frame = imutils.resize(frame, width=W)
+        (H, W) = frame.shape[:2]
+        if initBB is not None:
+            distx = int(initBB[2] - initBB[0])
+            disty = int(initBB[3] - initBB[1])
+            tracker.init(frame, initBB)
+            (success, box) = tracker.update(frame)
+            # check to see if the tracking was a success
+            if success:
+                (x, y, w, h) = [int(v) for v in box]
+                w = x+distx
+                h = y+disty
+                cv2.rectangle(frame, (x, y), (w, h),
+                    (0, 255, 0), 2)
+                
+                Testbox = [x, y, w, h]
+                if not math.isnan(gt[aux2][0]):
+                    Jack = jaccard(gt[aux2], Testbox)
+                    totalJack = np.append(totalJack, Jack)
+                    print("Jaccard index = ", Jack)
+                    if Jack == 0.0:
+                        error = error + 1
+                        print("Updating initial bounding box!")
+                        tracker.clear()
+                        tracker = OPENCV_OBJECT_TRACKERS[args["tracker"]]()
+                        initBB = tuple(gt[aux2])
+            elif not math.isnan(gt[aux2][0]):
+                tracker.clear()
+                tracker = OPENCV_OBJECT_TRACKERS[args["tracker"]]()
+                error = error + 1
+                initBB = tuple(gt[aux2])
 
-    # check to see if we are currently tracking an object
-    if initBB is not None:
-        # grab the new bounding box coordinates of the object
-        (success, box) = tracker.update(frame)
-        tracker.init(frame, initBB)
-        fps = FPS().start()
-        # check to see if the tracking was a success
-        if success:
-            (x, y, w, h) = [int(v) for v in box]
-            cv2.rectangle(frame, (x, y), (x + w, y + h),
-                (0, 255, 0), 2)
+            info = [
+                ("Tracker", args["tracker"]),
+                ("Success", "Yes" if success else "No"),
+                ("Jaccard Index","{:.3f}".format(Jack))
+            ]
 
-        # update the FPS counter
-        fps.update()
-        fps.stop()
 
-        # initialize the set of information we'll be displaying on
-        # the frame
-        info = [
-            ("Tracker", args["tracker"]),
-            ("Success", "Yes" if success else "No"),
-            ("FPS", "{:.2f}".format(fps.fps())),
-        ]
+            for (i, (k, v)) in enumerate(info):
+                text = "{}: {}".format(k, v)
+                cv2.putText(frame, text, (10, H - ((i * 20) + 20)),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
 
-        # loop over the info tuples and draw them on our frame
-        for (i, (k, v)) in enumerate(info):
-            text = "{}: {}".format(k, v)
-            cv2.putText(frame, text, (10, H - ((i * 20) + 20)),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
-    # show the output frame
-    cv2.imshow("Frame", frame)
-    key = cv2.waitKey(1) & 0xFF
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1) & 0xFF
 
-    # if the 's' key is selected, we are going to "select" a bounding
-    # box to track
-    if key == ord("s"):
-        # select the bounding box of the object we want to track (make
-        # sure you press ENTER or SPACE after selecting the ROI)
-        initBB = cv2.selectROI("Frame", frame, fromCenter=False,
-            showCrosshair=True)
-        print (initBB)
-        # start OpenCV object tracker using the supplied bounding box
-        # coordinates, then start the FPS throughput estimator as well
-        tracker.init(frame, initBB)
-        fps = FPS().start()
+        if key == ord("q"):
+            break
+        aux2 = aux2 + 1
 
-    # if the `q` key was pressed, break from the loop
-    elif key == ord("q"):
-        break
+    # close all windows
+    cv2.destroyAllWindows()
+    evaluate(gt, totalJack, error, args["tracker"], args["video"])
 
-# close all windows
-cv2.destroyAllWindows()
+if __name__ == '__main__':
+
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-v", "--video", type=str, default="car1",
+        help="path to input video file")
+    ap.add_argument("-t", "--tracker", type=str, default="mosse",
+        help="OpenCV object tracker type")
+    args = vars(ap.parse_args())
+
+    main1(ap, args)